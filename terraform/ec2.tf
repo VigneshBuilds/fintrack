2567a7c0d8c4ecbd2d1b06ce86e960108a434684
VigneshBuilds <vgnsh8225@gmail.com>
2025-05-14 14:43:41 +0000

    Fix ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "py_server" {
   ami                         = data.aws_ami.ubuntu.id
   associate_public_ip_address = true
   instance_type               = "t3.micro"
-  key_name                    = var.key_name
   user_data                   = file("./user_scripts/user_data.sh")
   subnet_id                   = module.network.public_sn_id
   vpc_security_group_ids      = [aws_security_group.allow_all_traffic.id]
